3ea5a04e18cdd7f8456afd56feedbcbbccd52930
Denis Rybalka <denis@boomstraat.de>
2025-04-14 10:21:56 +0300

    The initial raw meta data

---
+++ Gregarine.R
@@ -0,0 +1,134 @@
+# Installing packages
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("multcompView")
+install.packages("rstudioapi")
+
+# Load libraries
+library(dplyr)
+library(ggplot2)
+library(multcompView)
+library(rstudioapi)
+
+# Setting working directory to current
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+
+rawData <- read.csv("RawData.csv", skip= 11)
+
+# Un-comment compound variable one by one to get complete results
+
+# compound <- "Aniline"
+# compound <- "Formaldehyde"
+compound <- "Ortho-xylene"
+
+# Label localization
+language <- "en"
+compoundName <- switch(compound,
+                       "Aniline" = ifelse(language == "en", "aniline", "аніліну"),
+                       "Formaldehyde" = ifelse(language == "en", "formaldehyde", "формальдегіду"),
+                       "Ortho-xylene" = ifelse(language == "en", "ortho-xylene", "орто-ксилену"),
+                       NA  # Default case
+)
+
+
+labelXUA <- bquote("Концентрація " ~ .(compoundName) ~ "в кормовому субстраті, мг/кг")
+labelYUA <- expression("Зміна маси тіла " * italic("T. molitor") * ", мг/доба");
+
+labelXEN <- bquote("Concentration of " ~ .(compoundName) ~ " in the feed substrate, mg/kg")
+labelYEN <- expression("Body mass change of " * italic("T. molitor") * ", mg/day");
+
+if (language == "en") {
+  labelX <- labelXEN
+  labelY <- labelYEN
+} else {
+  labelX <- labelXUA
+  labelY <- labelYUA
+}
+
+
+# Filter the dataset for "chosen compound" and "Control"
+compoundData <- rawData %>% filter(Compound %in% c(compound, "Control"))
+colnames(compoundData) <- c("Compound", "Concentration", "StartWeight", "EndWeight", "GregarinaCount", "Change", "ChangePercent")
+compoundData <- compoundData %>% filter(!is.na(GregarinaCount))
+compoundData$Concentration <- factor(compoundData$Concentration, ordered = T)
+
+# Experiment lasted for 10 days
+compoundData$Change <- as.numeric(compoundData$Change) / 10
+
+# We need max values to get correct scales points
+maxValues <- compoundData %>%
+  group_by(Concentration) %>%
+  summarise(maxChange = ceiling(max(Change)))
+
+# Perform ANOVA
+anovaResult <- aov(Change ~ Concentration, data = compoundData)
+summary(anovaResult)
+
+# Tukey's HSD Test for pairwise comparisons
+tukeyResult <- TukeyHSD(anovaResult)
+summary(tukeyResult)  # Check pairwise comparison results
+letters <- multcompLetters4(anovaResult, tukeyResult)$Concentration$Letters
+
+# Counting speciments in each group
+groupCounts <- compoundData %>%
+  group_by(Concentration) %>%
+  summarise(N = n())
+
+updatedYLabels <- groupCounts %>%
+  mutate(label = paste0(Concentration, "\n(N=", N, ")"))
+
+# Prepare annotation data
+annotation_data <- data.frame(
+  Concentration = levels(compoundData$Concentration),
+  label = letters
+)
+
+annotation_data <- annotation_data %>%
+  left_join(maxValues, by = "Concentration")
+
+# Create the boxplot with annotations
+ggplot(compoundData, aes(x = Concentration, y = Change)) +
+  geom_boxplot(width = 0.2, outlier.shape = 8, outlier.color = "red", outlier.size = 2) +
+  stat_summary(
+    fun = mean, geom = "point", shape = 23, size = 3, fill = "blue", color = "black"
+  ) +  # Add mean points
+  stat_summary(
+    fun = mean, geom = "line",
+    aes(group = 1),  # Ensures all points are connected
+    color = "blue", size = 1
+  ) +
+  stat_summary(fun = mean, 
+               geom = "text", 
+               aes(label = round(..y.., 2)), 
+               hjust = -1, 
+               color = "blue"
+  ) +  # Mean labels
+  geom_jitter(
+    aes(color = Concentration), width = 0.2, size = 2, alpha = 0.6
+  ) +  # Add jittered points
+  scale_y_continuous(
+    breaks = seq(floor(min(compoundData$Change)), ceiling(max(compoundData$Change)), by = 1),
+    limits = c(min(as.numeric(as.character(compoundData$Change)) - 0.5), 
+               max(as.numeric(as.character(compoundData$Change))) + 0.5),
+  ) +  # Set y-axis scale to increment by 1
+  geom_text(
+    data = annotation_data,
+    aes(x = Concentration, y = maxChange + 0.2, label = label),
+    inherit.aes = FALSE,
+    size = 3.5
+  ) +
+  scale_x_discrete(labels = updatedYLabels$label) +  # Set custom labels
+  labs(
+    x = labelX,
+    y = labelY,
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  theme(
+    axis.text.x = element_text(size = 10, hjust = 0.5),
+    axis.text.y = element_text(size = 10, hjust = 0.5),
+    axis.title = element_text(size = 12, face = "bold"),
+    plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+    legend.position = "none"
+  )
---
+++ README.md
@@ -0,0 +1,39 @@
+# Effects of toxic organic compounds on Tenebrio molitor and its parasite Gregarina steini
+
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/continuous-foundation/fair-principles/blob/main/LICENSE)
+
+
+## Abstract
+
+Environmental pollution by toxic compounds affects various species of living organisms, their life cycles and symbiotic relationships. This study investigated the host-parasite relationship between Tenebrio molitor Linnaeus, 1758 (Coleoptera, Tenebrionidae) and Gregarina steini Berthold, 1827 (Eugregarinorida, Gregarinida) under exposure to o-xylene, formaldehyde and aniline at different concentrations. For laboratory studies, 480 larvae of T. molitor and five concentrations of the studied organic compounds were used. Groups of T. molitor, each consisting of ten individuals, were subjected to the tested compounds for 10 days following the initial weight measurement. We measured the body weight and survival rate of T. molitor and the number of G. steini in them to assess the impact of the tested organic compounds on the host-parasite relationship. For all the compounds studied, a decrease in body weight and high mortality were observed in T. molitor at high concentrations. The lethality of the studied organic compounds was concentration-dependent. o-Xylene showed low toxicity at lower concentrations (10.4 and 26.0 mg/kg of feed), with a survival rate of 93.3% and 86.7%, respectively. At a concentration of 10.4 mg/kg, no lethal cases in T. molitor were observed. At higher concentrations (78 and 104 mg/kg feed), T. molitor mortality significantly increased to 33.3% and 53.3%, respectively.
+
+
+### Material and methods
+
+The following repository contains all data and metadata for conductiong the next analyses:
+
+We conducted all statistical analyses using R version 4.4.2 (R Core Team, 2024) in
+RStudio version 2024.12.0+467. We used the following R packages: dplyr (version
+1.1.4), ggplot2 (version 3.5.1), and multcompView (version 0.1-10) for post hoc statistical
+analysis. We performed one-way analysis of variance (ANOVA) and Tukey’s HSD test to
+identify pairwise differences between pollutant concentrations, G. steini abundance, and
+changes in T. molitor body mass. We considered differences statistically significant at P
+&lt; 0.05.
+
+#### Keywords:
+
+Tenebrio molitor; Gregarina steini; parasitic systems; xylene; formaldehyde; aniline.
+
+#### Contributors:
+
+Project lead, biological and ecological reasoning:
+
+Viktor Brygadyrenko
+
+E-Mail: brigad@ua.fm
+
+Research and development, data visualization and aggregation:
+
+Denis Rybalka
+
+E-Mail: denisrybalka89@gmail.com
